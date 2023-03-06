2c4647f69c2e4fd296a5411cd88fbfbe15ace6dc
mrludovicc <nadal46@hotmail.com>
2023-03-06 18:06:54 -0500

    Adding another way to low average assingment WEEK-05 DAY-01

--- 3_queries_group-by/low_avg_completion_time.sql
+++ 3_queries_group-by/low_avg_completion_time.sql
@@ -1,11 +1,23 @@
-SELECT students.name as student,
-AVG(assignment_submissions.duration) AS average_assignment_duration,
-AVG(assignments.duration) AS average_estimated_duration
-FROM students
-JOIN assignment_submissions ON student_id = students.id
-JOIN assignments ON assignments.id = assignment_id
+-- SELECT students.name as student,
+-- AVG(assignment_submissions.duration) AS average_assignment_duration,
+-- AVG(assignments.duration) AS average_estimated_duration
+-- FROM students
+-- JOIN assignment_submissions ON student_id = students.id
+-- JOIN assignments ON assignments.id = assignment_id
+-- WHERE end_date IS NULL
+-- GROUP BY student
+-- HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+-- ORDER BY average_assignment_duration DESC;
+
+
+-- ANOTHER WAY --
+SELECT s.name as student,
+AVG(a_s.duration) AS average_assignment_duration,
+AVG(a.duration) AS average_estimated_duration
+FROM students s
+JOIN assignment_submissions a_s ON student_id = s.id
+JOIN assignments a ON a.id = assignment_id
 WHERE end_date IS NULL
 GROUP BY student
-HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
-ORDER BY average_assignment_duration DESC;
-
+HAVING AVG(a_s.duration) < AVG(a.duration)
+ORDER BY 2 DESC;
